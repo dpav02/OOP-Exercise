244ab5d19b7f4c835259c05368345e10af6c8cda
David Pavel <dpav02@davids-macbook-air.local>
2015-11-28 20:20:34 -0500

    Initial Commit

---
+++ OOP-Exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OOP-Exercise
+//
+//  Created by David Pavel on 11/28/15.
+//  Copyright © 2015 David Pavel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
